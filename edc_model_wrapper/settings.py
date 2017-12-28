0e7a9eada54ecfc38112a20e9dfbcdf4954d28bb
erikvw <ew2789@gmail.com>
2017-12-28 21:10:37 +0200

    update settings

--- edc_model_wrapper/settings.py
+++ edc_model_wrapper/settings.py
@@ -4,6 +4,8 @@ import sys
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 APP_NAME = 'edc_model_wrapper'
+ETC_DIR = os.path.join(BASE_DIR, 'etc')
+SITE_ID = 40
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -26,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'edc_base.apps.AppConfig',
     'edc_protocol.apps.AppConfig',
     'edc_device.apps.AppConfig',
